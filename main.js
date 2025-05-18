268e18f848c43e9311e1a9fef2c265b0cca6cc10
c-bejar <p.bejar.christopher@gmail.com>
2025-05-18 00:35:25 -0500

    basic layout complete

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-const cardContainer = document.querySelector(".card-container");
-
 function createCard(cardData) {
-  const cardElement = document.createElement("div");
+  const cardElement = document.createElement("img");
+  cardElement.src = cardData.image_url;
+  cardElement.alt = cardData.name;
   cardElement.className = "card";
-
-  cardElement.innerHTML = `
-    <img src="${cardData.image_url}" alt="${cardData.name}" width="125">
-  `;
 
   return cardElement;
 }
@@ -14,20 +10,20 @@ function createCard(cardData) {
 async function loadCards() {
   try {
     const response = await fetch("cards.json");
-    if (!response.ok) throw new Error("unable to fetch cards.json");
+    if (!response.ok) throw new Error("Unable to fetch cards.");
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
-    console.error("Error loading cards: ", error);
+    console.error("Error loading cards:", error);
     return null;
   }
 }
 
+const cardContainer = document.querySelector(".card-container");
 const loadedCards = loadCards();
 
 loadedCards.then((cards) => {
-  cards.forEach((card) => {
+  cards.map((card, index) => {
     cardContainer.appendChild(createCard(card));
   });
 });
