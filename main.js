122e19e7d9cf7bf8240ab6eb314708c7b728aa2f
c-bejar <p.bejar.christopher@gmail.com>
2025-05-18 23:49:49 -0500

    deck example + filter options [nonfunctional]

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-function createCard(cardData) {
+function createCard(cardData, toDeck = false) {
   const cardElement = document.createElement("img");
   cardElement.src = cardData.image_url;
   cardElement.alt = cardData.name;
-  cardElement.className = "card";
+  if (toDeck) {
+    cardElement.className = "deck-card";
+  } else {
+    cardElement.className = "card";
+  }
+  cardElement.loading = "lazy";
 
   return cardElement;
+}
+
+function createOption(setName) {
+  const option = document.createElement("option");
+  option.text = setName;
+  return option;
 }
 
 async function loadCards() {
@@ -20,10 +31,20 @@ async function loadCards() {
 }
 
 const cardContainer = document.querySelector(".card-container");
+const deckContainer = document.querySelector(".deck-container");
+const setContainer = document.querySelector('select[name="Card Set"]');
 const loadedCards = loadCards();
+let cardSets = new Set();
 
 loadedCards.then((cards) => {
   cards.map((card, index) => {
+    cardSets.add(card.set);
+    if (index < 50) {
+      deckContainer.appendChild(createCard(card, true));
+    }
     cardContainer.appendChild(createCard(card));
+  });
+  cardSets.forEach((cardSet) => {
+    setContainer.appendChild(createOption(cardSet));
   });
 });
