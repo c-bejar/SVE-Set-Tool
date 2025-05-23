31cc6536c6031de27f8deee6a16405fc91dea1a6
c-bejar <p.bejar.christopher@gmail.com>
2025-05-23 02:34:01 -0500

    alt copy text solution for tts

--- main.js
+++ main.js
@@ -335,9 +335,25 @@ let universes = new Set();
 
 copyButton.addEventListener("click", function (e) {
   const text = grabDeckText();
-  navigator.clipboard.writeText(text).then(() => {
-    alert("Wrote to clipboard.");
-  });
+  try {
+    navigator.clipboard.writeText(text).then(() => {
+      console.log("Wrote to clipboard");
+    });
+  } catch (err) {
+    console.error("Error: ", err);
+
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+      document.execCommand("copy");
+    } catch (fallbackErr) {
+      console.error("Error: ", fallbackErr);
+    }
+    document.body.removeChild(textArea);
+  }
 });
 tokenButton.addEventListener("click", function (e) {
   let clip = "0 ([_]) .\n";
